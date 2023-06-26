6918113c2889f472801fdebb68e860ac914c52b7
Hendrik Boeck <hendrikboeck.dev@protonmail.com>
2023-06-26 13:12:47 +0200

    improve ip:port handling

--- src/command_builder.rs
+++ src/command_builder.rs
@@ -11,17 +11,23 @@ use crate::yaml::{Host, SshOptions};
 
 pub static LOG_FILE: OnceCell<String> = OnceCell::new();
 
-fn format_ip(addr_or_port: &String) -> String {
-    if addr_or_port.contains(':') {
-        addr_or_port.clone()
-    } else {
-        format!("localhost:{}", addr_or_port)
+trait ToSshAddr {
+    fn to_ssh_addr(&self) -> Self;
+}
+
+impl ToSshAddr for String {
+    fn to_ssh_addr(&self) -> Self {
+        if self.contains(':') {
+            self.clone()
+        } else {
+            format!("localhost:{self}")
+        }
     }
 }
 
 pub fn build_ssh_process(name: &str, host: &Host) -> Command {
     let ssh_opt_default = SshOptions::default();
-    let ssh_opt = host.ssh_options.clone().unwrap_or_default();
+    let ssh_opt = host.ssh_options.as_ref().unwrap_or(&ssh_opt_default);
 
     info!("Building SSH command for host {}", &name.green());
     let mut cmd = Command::new("ssh");
@@ -29,6 +35,12 @@ pub fn build_ssh_process(name: &str, host: &Host) -> Command {
     cmd.stdin(Stdio::null())
         .stdout(Stdio::null())
         .stderr(Stdio::null());
+
+    cmd.arg("-N")
+        .arg("-o")
+        .arg("ControlPersist=yes")
+        .arg("-o")
+        .arg("BatchMode=yes");
 
     if let Some(connect_timeout) = ssh_opt.connect_timeout.or(ssh_opt_default.connect_timeout) {
         cmd.arg("-o")
@@ -43,12 +55,6 @@ pub fn build_ssh_process(name: &str, host: &Host) -> Command {
             .arg(format!("ServerAliveInterval={}", server_alive_interval));
     }
 
-    cmd.arg("-N")
-        .arg("-o")
-        .arg("ControlPersist=yes")
-        .arg("-o")
-        .arg("BatchMode=yes");
-
     // set log path
     let log = TMP_DIR.join(format!("ant_{}.log", Local::now().to_rfc3339()));
     LOG_FILE.set(log.to_str().unwrap().to_string()).unwrap();
@@ -56,29 +62,36 @@ pub fn build_ssh_process(name: &str, host: &Host) -> Command {
 
     if let Some(ref local_forward) = host.local_forward {
         for fwd in local_forward.iter() {
+            let remote = fwd.remote.to_ssh_addr();
+            let local = fwd.local.to_ssh_addr();
+
             info!(
                 "Added local port forwarding from (R) {} to (L) {}",
-                format_ip(&fwd.remote).green(),
-                format_ip(&fwd.local).green(),
+                &remote.green(),
+                &local.green(),
             );
-            cmd.arg("-L").arg(format!("{}:{}", &fwd.local, &fwd.remote));
+            cmd.arg("-L").arg(format!("{}:{}", &local, &remote));
         }
     }
 
     if let Some(ref remote_forward) = host.remote_forward {
         for fwd in remote_forward.iter() {
+            let remote = fwd.remote.to_ssh_addr();
+            let local = fwd.local.to_ssh_addr();
+
             info!(
                 "Added remote port forwarding from (L) {} to (R) {}",
-                format_ip(&fwd.local).green(),
-                format_ip(&fwd.remote).green(),
+                &local.green(),
+                &remote.green(),
             );
-            cmd.arg("-R").arg(format!("{}:{}", &fwd.remote, &fwd.local));
+            cmd.arg("-R").arg(format!("{}:{}", &remote, &local));
         }
     }
 
-    if let Some(ref strict_host_key_checking) = ssh_opt
+    if let Some(strict_host_key_checking) = ssh_opt
         .strict_host_key_checking
-        .or(ssh_opt_default.strict_host_key_checking)
+        .as_ref()
+        .or(ssh_opt_default.strict_host_key_checking.as_ref())
     {
         cmd.arg("-o");
         match strict_host_key_checking as _ {
@@ -98,7 +111,11 @@ pub fn build_ssh_process(name: &str, host: &Host) -> Command {
         };
     }
 
-    if let Some(ref bind_address) = ssh_opt.bind_address.or(ssh_opt_default.bind_address) {
+    if let Some(bind_address) = ssh_opt
+        .bind_address
+        .as_ref()
+        .or(ssh_opt_default.bind_address.as_ref())
+    {
         cmd.arg("-b").arg(&bind_address);
     }
 
@@ -109,11 +126,15 @@ pub fn build_ssh_process(name: &str, host: &Host) -> Command {
         ));
     }
 
-    if let Some(ref ciphers) = ssh_opt.ciphers.or(ssh_opt_default.ciphers) {
+    if let Some(ciphers) = ssh_opt
+        .ciphers
+        .as_ref()
+        .or(ssh_opt_default.ciphers.as_ref())
+    {
         cmd.arg("-o").arg(format!("Ciphers={}", &ciphers));
     }
 
-    if let Some(ref macs) = ssh_opt.macs.or(ssh_opt_default.macs) {
+    if let Some(macs) = ssh_opt.macs.as_ref().or(ssh_opt_default.macs.as_ref()) {
         cmd.arg("-o").arg(format!("Macs={}", &macs));
     }
 
